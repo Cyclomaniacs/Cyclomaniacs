bc75f915042acdd662e69d080f22b41892ae2ebe
n-norrr <norlandbatista@ufl.edu>
2021-04-05 15:24:25 -0400

    split into individual retailer, product, and sells databases

--- backend/cyclescrapers/cyclescrapers/pipelines.py
+++ backend/cyclescrapers/cyclescrapers/pipelines.py
@@ -19,11 +19,22 @@ class CyclescrapersPipeline:
         )
         self.cur = self.connection.cursor()
         self.cur.execute("""
-            CREATE TABLE IF NOT EXISTS cyclo_items (
-                name varchar(255),
-                price varchar(255),
-                retailer varchar(255),
-                url varchar(255)
+            CREATE TABLE IF NOT EXISTS cyclo_retailer (
+                name        varchar(255) PRIMARY KEY,
+                link        varchar(255)
+            )
+        """)
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS cyclo_product (
+                link        varchar(255) PRIMARY KEY,
+                name        varchar(255),
+                price       varchar(255)
+            )
+        """)
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS sells (
+                retailer    varchar(255),
+                product     varchar(255)
             )
         """)
         self.connection.commit()
@@ -35,7 +46,18 @@ class CyclescrapersPipeline:
 
 
     def process_item(self, item, spider):
-        self.cur.execute("insert into cyclo_items(name, price, retailer, url) values(%s, %s, %s, %s)", (item['name'], item['price'], item['retailer'], item['url']))
+        self.cur.execute("""
+            INSERT INTO cyclo_product(link, name, price) values(%s, %s, %s)
+        """, (item['url'], item['name'], item['price'])
+        )
+        self.cur.execute("""
+            INSERT INTO cyclo_retailer(name, link) values(%s, %s)
+        """, (item['retailer'], item['url'])
+        )
+        self.cur.execute("""
+            INSERT INTO sells(retailer, product) values(%s, %s)
+        """, (item['retailer'], item['name'])
+        )
         self.connection.commit()
         print(item['name'])
         print(item['price'])
