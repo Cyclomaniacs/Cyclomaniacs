90943a2a702711b6fbbbcf0576324bfe6a1008d2
csaflip <colin.adams@ufl.edu>
2021-04-13 17:15:47 -0400

    got router to work

--- frontend/cycloman/src/App.js
+++ frontend/cycloman/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import about from './components/about';
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+		<Router>
+			<Container>
+				<Route exact path="/" component={about} />
+				{/* <Route exact path="/forgotPW" component={forgotPW} />
+				<Route exact path="/adminHome" component={adminHome} />
+				<Route exact path="/orderSum" component={orderSum} />
+				<Route exact path="/resetPW/*" component={resetPW} /> */}
+			</Container>
+		</Router>
   );
 }
+
+// export default App;
+// import React from 'react';
+// import { Container } from 'semantic-ui-react';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import 'semantic-ui-css/semantic.min.css';
+// import adminHome from './Pages/adminHome';
+// import orderSum from './Pages/orderSum';
+// import forgotPW from './Pages/forgotPW';
+// import loginScreen from './Pages/loginScreen';
+// import resetPW from './Pages/resetPW';
+
+// function App() {
+// 	return (
+
+// 	);
+// }
 
 export default App;
---
+++ frontend/cycloman/src/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function About() {
+    return (
+        <h1>Hello World!</h1>
+    )
+}
+
+
+export default About;
